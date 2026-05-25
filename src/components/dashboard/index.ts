fd330bd65d49576c74a5b0bdd24a111300c420a8
iljoung oh <70251881+happy8131@users.noreply.github.com>
2026-05-25 21:21:37 +0900

    feat: 메인 대시보드 페이지 통합 (모든 컴포넌트 연결)

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,1 +1,12 @@
-export {};
+export { default as StatCard } from './StatCard';
+export { default as EmissionChart } from './EmissionChart';
+export { default as SourceChart } from './SourceChart';
+export { default as CompanyStackChart } from './CompanyStackChart';
+export { default as CompanyTable } from './CompanyTable';
+export {
+  SkeletonCard,
+  SkeletonChart,
+  SkeletonTable,
+  ErrorScreen,
+  LoadingScreen,
+} from './Skeleton';
